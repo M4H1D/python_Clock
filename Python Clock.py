cba5d224f589066c8a05b7b430b867a0f2110ab9
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-01-05 15:53:17 +0600

    Update Python Clock.py

--- Python Clock.py
+++ Python Clock.py
@@ -2,7 +2,7 @@ from tkinter import *
 from time import strftime
 root=Tk()
 def time():
-	clock=strftime("%I:%M:%S %p")
+	clock=strftime("%I:%M:%S %p")   #if you want 24 Hour clock just type %H in %I position...(%I=12 hour)& (%H=24 hour)
 	label.configure(text=clock)
 	label.after(1000,time)
 label=Label(root,fg="green",bg="Black",height=40,width=80)
