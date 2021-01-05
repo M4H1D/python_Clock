36b4865ab57d4fdf0cac957ff01c1069d7ed9c02
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-01-05 15:45:28 +0600

    Update Python Clock.py

--- Python Clock.py
+++ Python Clock.py
@@ -2,7 +2,7 @@ from tkinter import *
 from time import strftime
 root=Tk()
 def time():
-	clock=strftime("%H:%M:%S %p")
+	clock=strftime("%I:%M:%S %p")
 	label.configure(text=clock)
 	label.after(1000,time)
 label=Label(root,fg="green",bg="Black",height=40,width=80)
